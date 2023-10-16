663ac127a82653b9cc37d69c0e37a6f9b6a8b0a3
Ahmad Alali <aalali>
2023-10-16 17:59:59 +0300

    fixed another errorgit add .git add .

--- functions/sortList.go
+++ functions/sortList.go
@@ -1,24 +1,47 @@
 package pushswap
 
-import "strconv"
+import (
+	"strconv"
+)
 
 var Combination string
 var FinalStackA string
 
 func SortList(stackA []int) string {
+	startingStackA := stackA
 	var stackB []int
 	switchP := 1
 	for i := 0; i >= 0; i++ {
 		oldStackA := stackA
 		oldStackB := stackB
+		stackA, stackB = ChooseFunc(stackA, stackB, S)
+		stackA, stackB = ChooseFunc(stackA, stackB, R)
+		stackA, stackB = ChooseFunc(stackA, stackB, RR)
 		stackA, stackB = ChooseFunc(stackA, stackB, R)
 		stackA, stackB = ChooseFunc(stackA, stackB, S)
 		stackA, stackB = ChooseFunc(stackA, stackB, RR)
 		if TestFinalList(stackA, "A") && TestFinalList(stackB, "B") {// if both stacks are arranged break
-			break
+			for i := 0; i >= 0; i++ {
+				var testA bool
+				if len(stackB) == 0 {// return all the values from stackb to stacka
+					break
+				}
+				stackB, stackA = P(stackB, stackA)
+				Combination = Combination + "pa\n"
+				stackA, testA, _ = S(stackA, "A")
+				if testA {
+					Combination = Combination + "sa\n"
+				}
+			}
+			if TestFinalList(stackA, "A") && len(stackA) == len(startingStackA) {
+				break
+			}
+			switchP++
 		}
-		if Testlist(oldStackA, stackA, "A") || Testlist(oldStackB, stackB, "B") {// check if the stack changed if so go through the functions again
-			i--
+		if Testlist(oldStackA, stackA, "A") {
+			continue
+		}
+		if (len(stackB) > 0) && Testlist(oldStackB, stackB, "B") {// check if the stack changed if so go through the functions again
 			continue
 		} else {
 			if switchP%2 == 0 {
@@ -34,18 +57,6 @@ func SortList(stackA []int) string {
 				stackA, stackB = P(stackA, stackB)
 				Combination = Combination + "pb\n"
 			}
-		}
-	}
-	for i := 0; i >= 0; i++ {
-		var testA bool
-		if len(stackB) == 0 {// return all the values from stackb to stacka
-			break
-		}
-		stackB, stackA = P(stackB, stackA)
-		Combination = Combination + "pa\n"
-		stackA, testA, _ = S(stackA, "A")
-		if testA {
-			Combination = Combination + "sa\n"
 		}
 	}
 	for i := 0; i < len(stackA); i++ {
